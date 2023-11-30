d8ca8f6dc6138d3e2fb7b3217ad2352b5801d409
Ethan Nguyen <nguyenethan01@gmail.com>
2023-11-30 00:45:54 -0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -2,3 +2,10 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    webpack: (config) => {
+      config.resolve.fallback = { fs: false, net: false, tls: false };
+      return config;
+    },
+  };
